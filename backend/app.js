ca8d9cfe6942e519343bdd430d5dd27a59802862
MaevaPrecourt <maeva.precourt@gmail.com>
2023-02-22 17:09:15 +0100

    Dotenv - 22/2

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,17 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 app.use(express.json());
+const sauce = require("./models/sauce");
 
 mongoose.connect("mongodb+srv://MaevaPrecourt:Maeva1996@piiquante.x6lztnj.mongodb.net/?retryWrites=true&w=majority",
-{useNewUrlParser: true,
+{
+    useNewUrlParser: true,
     useUnifiedTopology: true
 })
+
 .then(() => console.log("Connexion à MongoDB réussie !"))
 .catch(() => console.log("Connexion à MongoDB échouée !"));
 
