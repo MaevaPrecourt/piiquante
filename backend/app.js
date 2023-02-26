98df32339eceb5193560a215a23e67196b7831a0
MaevaPrecourt <maeva.precourt@gmail.com>
2023-02-26 16:06:20 +0100

    No sauces to display - 26/2

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,12 @@ require("dotenv").config;
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+
 const sauceRoutes = require("./routes/sauce");
 const userRoutes = require("./routes/user");
+
+const app = express();
+app.use(express.json());
 
 mongoose.connect("mongodb+srv://MaevaPrecourt:Maeva1996@piiquante.x6lztnj.mongodb.net/?retryWrites=true&w=majority",
 {
@@ -14,18 +18,15 @@ mongoose.connect("mongodb+srv://MaevaPrecourt:Maeva1996@piiquante.x6lztnj.mongod
 .then(() => console.log("Connexion à MongoDB réussie !"))
 .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-const app = express();
-
-app.use(function(request, response, next){
+app.use((request, response, next) => {
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
     response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
     next();
 });
 
-app.use(express.json());
-app.use("api/sauces", sauceRoutes);
-app.use("/api/auth", userRoutes);
 app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/api/sauces", sauceRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,12 +2,17 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (request, response, next) => {
     try{
-        const token = request.headers.authorization.split("")[1];
+        const token = request.headers.authorization.split(" ")[1];
         const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
         const userId = decodedToken.userId;
         request.auth = {
             userId: userId
         };
+        if(request.body.userId && request.body.userId != userId){
+            throw "Non autorisé.";
+        }else{
+            next();
+        }
     }catch(error){
         response.status(401).json({error});
     }
